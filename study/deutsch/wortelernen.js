042f0eb3f79b82132b8abf26217ad6d9c0e5592e
Ain Bolaños Cortés <88357657+0kron@users.noreply.github.com>
2023-03-15 05:55:15 +0000

    Add files via upload

---
+++ study/deutsch/wortelernen.js
@@ -0,0 +1,66 @@
+let artikel = ["der", "das", "die"];
+
+let k6_worter = [
+    ["Tätigkeit", "Actividad/Ocupación", 2, 0],
+    ["Termin", "Cita/Plazo", 0, 0],
+    ["Ärger", "Enojo", 0, 0],
+    ["Bahn", "Tren", 2, 0],
+    ["Fahrplan", "Horario/Itinerario", 0, 0],
+    ["Zugverbindung", "Enlace de Tren", 2, 0],
+    ["Durchsage", "Mensaje", 2, 0],
+    ["Wagen", "Vehículo", 0, 0],
+    ["Geschäftreise", "Viaje de Negocios", 2, 0],
+    ["Hinfahrt", "Viaje de ida", 2, 0],
+    ["Rückfahrt", "Viaje de regreso", 2, 0],
+    ["Klasse", "Clase", 2, 0],
+    ["Fahrkarte", "Pasaje", 2, 0],
+    ["Gang", "Pasillo", 0, 0],
+    ["Ermäßigung", "Rebaja", 2, 0],
+    ["Band", "Banda", 2, 0],
+    ["Musiker", "Músico", 0, 0],
+    ["Sängerin", "Cantante", 2, 0],
+    ["Alben", "Álbum", 1, 0],
+    ["Trainer", "Entrenador", 0, 0],
+    ["Berufswunsch", "Aspiración Profesional", 0, 0],
+    ["Neuanfang", "Reinicio", 0, 0],
+    ["Chance", "Oportunidad", 2, 0],
+    ["Umwelt", "Entorno", 2, 0],
+    ["Plastik", "Plástico", 1, 0],
+    ["Gehalt", "Sueldo", 1, 0],
+    ["Risiko/Risiken", "Riesgo", 1, 0],
+    ["Übersetzerin", "Traductor", 2, 0],
+    ["Chirurg", "Cirujano", 0, 0],
+
+];
+
+let k6_gelernt = [];
+
+document.getElementById("der").onclick = function(){
+    check();
+}
+
+document.getElementById("das").onclick = function(){
+    check();
+}
+
+document.getElementById("die").onclick = function(){
+    check();
+}
+
+
+
+function check(){
+
+};
+
+function rmWorter(){
+
+};
+
+function gelernt(){
+
+};
+
+function vergesen(){
+
+};
